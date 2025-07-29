6e7c3802b4b832afe3a34055fd0b4572375e20f3
Shuu Wolf <shuuwolf.home@gmail.com>
2025-07-29 18:05:10 -0300

    feat: New Images on case study #1

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,23 +9,6 @@ window.onload = (event) => {
         }
         e.target.classList.toggle('active');
     });
-
-    let atualPx = 16;
-    let outfont = document.getElementById("outfont");
-    let infont = document.getElementById("infont");
-    let numberPx = document.getElementsByTagName("number")
-
-    outfont.addEventListener("click", () => {
-        atualPx = atualPx - 0.5;
-        numberPx[0].innerHTML = atualPx + "px";
-        document.getElementsByClassName("xx")[0].style.fontSize = atualPx + "px";
-    })
-
-    infont.addEventListener("click", () => {
-        atualPx = atualPx + 0.5;
-        numberPx[0].innerHTML = atualPx + "px";
-        document.getElementsByClassName("xx")[0].style.fontSize = atualPx + "px";
-    })
 };
 
 
